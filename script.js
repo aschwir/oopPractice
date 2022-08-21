519cd559b48167507858d2e004ef925d4b16dcea
andrew schwirzinski <aschwirzinski@gmail.com>
2022-08-21 14:38:37 -0400

    coding challenges

--- script.js
+++ script.js
@@ -108,64 +108,184 @@
 // const PersonCL = class {}
 
 //class declaration
-class PersonCL {
+// class PersonCL {
    
-    constructor(fullName, birthYear){
-        this.fullName = fullName;
-        this.birthYear = birthYear;
-    }
+//     constructor(fullName, birthYear){
+//         this.fullName = fullName;
+//         this.birthYear = birthYear;
+//     }
 
 
-    calcAge(){
-        console.log(2037 - this.birthYear);
-    }
+//     calcAge(){
+//         console.log(2037 - this.birthYear);
+//     }
 
-    get age(){
-        return 2037 - this.birthYear
-    }
+//     get age(){
+//         return 2037 - this.birthYear
+//     }
 
-    set fullName(name){
-        console.log(name);
-        if(name.includes(' ')) this._fullName = name;
-        else alert(`${name} is not a full name!`)
-    }
+//     set fullName(name){
+//         console.log(name);
+//         if(name.includes(' ')) this._fullName = name;
+//         else alert(`${name} is not a full name!`)
+//     }
 
-    get fullName(){
-        return this._fullName;
-    }
-}
-
-
-const jessica = new PersonCL('Jessica Davis', 1996);
-console.log(jessica);
-jessica.calcAge();
-console.log(jessica.age);
-
-const walter = new PersonCL('Walter White', 1965)
+//     get fullName(){
+//         return this._fullName;
+//     }
+// }
 
 
-const account = {
-    owner: 'Jonas',
-    movements: [200,530,120,300],
+// const jessica = new PersonCL('Jessica Davis', 1996);
+// console.log(jessica);
+// jessica.calcAge();
+// console.log(jessica.age);
 
-    get latest(){
-        return this.movements.slice(-1).pop();
-    },
-
-    set latest(mov){
-        this.movements.push(mov);
-    },
-};
-
-console.log(account.latest);
-
-account.latest = 50
-
-console.log(account.movements);
+// const walter = new PersonCL('Walter White', 1965)
 
 
+// const account = {
+//     owner: 'Jonas',
+//     movements: [200,530,120,300],
+
+//     get latest(){
+//         return this.movements.slice(-1).pop();
+//     },
+
+//     set latest(mov){
+//         this.movements.push(mov);
+//     },
+// };
+
+// console.log(account.latest);
+
+// account.latest = 50
+
+// console.log(account.movements);
+
+
+// Object.create
+
+// const PersonProto = {
+//     calcAge(){
+//      console.log(2037 - this.birthYear);
+//      },
+
+//      init(firstName, birthYear){
+//         this.firstName = firstName;
+//         this.birthYear = birthYear;
+//      }
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
+// steven.calcAge();
+
+// console.log(steven.__proto__);
+
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
 
 
 
+// class Car{
+//     constructor(make, speed){
+//         this.make = make;
+//         this.speed = speed;
+//     }
 
 
+//     get speedUS(){
+//         return this.speed / 1.6
+//     }
+
+//     set speedUS(mph){
+//         this.speed = mph * 1.6 ;
+       
+//     }
+
+//   accelerate() {
+//         return `${this.speed += 10} km/h`;
+//     }
+
+//     brake() {
+//         return `${this.speed -= 5} km/h`
+//     };
+
+   
+
+// }
+
+// const ford = new Car('Ford', 120);
+
+
+
+// const Person = function(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+
+// };
+
+
+// Person.prototype.calcAge =function(){
+//     console.log(2037 - this.birthYear);
+// };
+
+// const Student = function(firstName,birthYear,course){
+//     Person.call(this,firstName,birthYear);
+//     // this.firstName = firstName;
+//     // this.birthYear = birthYear;
+//     this.course = course;
+// }
+
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function(){
+//     console.log(`My name is ${this.firstName} and I am studying ${this.course}`);
+// }
+
+// const mike = new Student('Mike', 2020, 'Computer Science');
+
+// mike.introduce();
+// mike.calcAge();
+
+// Student.prototype.constructor = Student;
+
+// console.log(mike.__proto__);
+
+
+
+// const Car = function(make, speed){
+//     this.make = make;
+//     this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function(){
+//     return `${this.speed += 10} km/h`;
+// };
+
+// Car.prototype.brake = function(){
+//     return `${this.speed -= 5} km/h`
+// };
+
+// const EV = function(make,speed,charge){
+//     Car.call(this,make,speed);
+//     this.charge = charge;
+// }
+
+// EV.prototype = Object.create(Car.prototype)
+
+// EV.prototype.chargeBattery = function(chargeTo){
+//     this.charge = chargeTo;
+// }
+
+// EV.prototype.accelerate = function(){
+//     this.speed += 20;
+//     this.charge -= 1;
+//     console.log(`${this.make} going at ${this.speed}km/h, with a charge of ${this.charge}%`);
+// }
+
+// const tesla = new EV('Tesla', 120, 23);
